ce08e2f6ec5de9476228589fa1e422856f7532d1
aadityar <aaditya_rao@outlook.com>
2023-09-12 14:04:07 -0700

    updated api to support data fetching

--- api/index.py
+++ api/index.py
@@ -10,6 +10,10 @@ from langchain.schema import Document
 from langchain.chat_models import ChatAnthropic
 from pydantic import BaseModel
 
+# load api keys *required for vercel deployments*
+from dotenv import load_dotenv
+load_dotenv()
+
 # import api keys and init models
 PINECONE_KEY = os.environ.get("pinecone_key")
 COHERE_KEY = os.environ.get("cohere_key")
@@ -29,61 +33,12 @@ class query(BaseModel):
     index: str
     chat_history: list
 
-class index(BaseModel):
+class Index(BaseModel):
     name: str
 
 
-#search function
-# def search(question, namespace, index, chat_history=[], model='anthropic', k=4):
-#     """Search pinecone index for a given question.
-
-#     Args:
-#         question: The question being answered
-#         namespace: The namespace to search
-#         index: The index in which the specified namespace is contained
-#         chat_history: The chat history formatted as [("Q", "A"), ("Q", "A")...]
-#         model: The model used to interpret the question (choose from "palm" or "anthropic")
-#         k: The number of responses returned
-
-#     Returns:
-#         tuple: The delay in seconds and the output as a dictionary
-
-#             (
-#                 delay, 
-#                 {
-#                     "question": question, 
-#                     "result": result, 
-#                     "documents": 
-#                         [
-#                             {"content": content, "source": source,}
-#                             {"content": content, "source": source,}
-#                                             ...
-#                         ]
-#                 }
-#             )
-#     """
-
-
-#     docsearch = Pinecone.from_existing_index(index, embedding_model, namespace=namespace)
-#     chat_history = [(item[0], item[1]) for item in chat_history]
-
-#     qa = ConversationalRetrievalChain.from_llm(llm,docsearch.as_retriever(search_kwargs=dict(k=k)), return_source_documents=True,)
-
-#     start = time.perf_counter()
-#     xc = qa({"question": question, "chat_history": chat_history})
-#     delay = time.perf_counter()-start
-
-
-#     #format output
-#     output = {"question": question, "result": xc["answer"], "documents": [{"content": doc.page_content, "source": doc.metadata} for doc in xc["source_documents"]]}
-#     output["delay"] = delay 
-#     return (delay, output)
-
-
-
-@app.post("/api/server/query")
-async def searchexe(request_body: index):
-    #output = search(query.question, query.namespace, query.index, query.chat_history)
-    output = {"name": request_body.name,
-              "test": 1,}
-    return output
+@app.post("/api/server/query")  # You might want to use POST for creating resources
+async def search_exe(request_body: Index):
+    index_dict = request_body.dict()
+    index_dict["status"] = "success"  # Add "status" field to the response
+    return index_dict
